e9813c89b1e4bed09644bfa0cf3c2893de1deee8
felipeselau <felipeselau68@gmail.com>
2024-08-15 20:08:00 -0300

    added empty directory verification

--- dirsearch.ps1
+++ dirsearch.ps1
@@ -2,4 +2,24 @@
 # requires fzf and fd
 # Usage: set and alias and have fun
 
-fd  -t d -d 1 | fzf --preview 'fd . {} -d 1' | Set-Location 
+$nSubfolders = (Get-ChildItem -Directory).Count;
+
+
+if($nSubfolders -eq 0) {
+    $search = fd;
+    $preview = 'bat {}'
+}
+
+if ($nSubfolders -ge 1) {
+    $search = fd  -t d -d 1;
+    $preview = 'fd . {} -d 1'
+}
+
+
+if (!($search) || !($preview)) {
+    Write-Output "Empty Directory";
+    exit;
+}
+
+
+$search | fzf --preview $preview | Set-Location 
